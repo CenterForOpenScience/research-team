cb49efbada711b45ec876a3fb57c563ef2d9dbc3
Hannah Moshontz <hannahmoro@users.noreply.github.com>
2022-12-19 15:59:28 -0800

    Create analyze_retro_pilot_v2.R
    initial commit + basic set up

---
+++ r3ct/analyze_retro_pilot_v2.R
@@ -0,0 +1,10 @@
+# this script analyzes the results of the retro pilot
+# conducted using v2 of the R and R decision letter coding scheme
+
+library(tidyverse)
+library(googledrive)
+
+#source function scripts
+source("../tools/fn_gdrive.R")
+
+#
