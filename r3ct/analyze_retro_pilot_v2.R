b5420a1d4b3e1980ceae5a3c197289479a2d76d8
Hannah Moshontz <hannahmoro@users.noreply.github.com>
2022-12-21 11:46:46 -0600

    Update analyze_retro_pilot_v2.R
    import and merge coding data

--- r3ct/analyze_retro_pilot_v2.R
+++ r3ct/analyze_retro_pilot_v2.R
@@ -3,8 +3,58 @@
 
 library(tidyverse)
 library(googledrive)
+library(googlesheets4)
+library(purrr)
+
+###### functions
 
 #source function scripts
 source("../tools/fn_gdrive.R")
 
-#
+#get spreadsheet
+
+get_retro_inclusion_pilot_sheet_id <- function(version) {
+
+dribble <- research_drive_ls("Projects") %>%
+  pull(id) |>
+  drive_ls("R3CT") |>
+  pull(id) |>
+  drive_ls("Methods") |>
+  pull(id) |>
+  drive_ls("retrospective_pilots") |>
+  pull(id) |>
+  drive_ls("coding", type = "spreadsheet")
+
+id <- dribble |>
+  filter(str_detect(name, version)) |>
+  pull(id)
+
+return(id)
+
+}
+
+
+###### import data
+
+get_retro_inclusion_pilot_sheet_id("v2") |>
+  sheet_properties()
+
+coder1 <- get_retro_inclusion_pilot_sheet_id("v2") |>
+  read_sheet("macie") |>
+  mutate(coder = 1) |>
+  glimpse()
+
+coder2 <- get_retro_inclusion_pilot_sheet_id("v2") |>
+  read_sheet("hannah") |>
+  mutate(coder = 2,
+         decision_date = as.Date(decision_date)) |>
+  glimpse()
+
+##### wrangle data
+
+pilot_v2 <- coder1 |>
+  full_join(coder2)
+
+#clean up
+rm(coder1, coder2)
+
